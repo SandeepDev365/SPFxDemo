6e8a8ccd4bceb95ba74fdd26450a4374c7133c5f
SandeepDev365 <93072655+SandeepDev365@users.noreply.github.com>
2021-11-27 20:15:10 +0530

    added ps

---
+++ DeploySPFxWP.ps1
@@ -0,0 +1,10 @@
+﻿Write-Output "Executing PowerShell Script..."
+
+$test = $Env:DEVSITEURL
+Write-Output "Test secret - $test"
+
+#Install-Module -Name PnP.PowerShell -Force -Scope "CurrentUser"
+
+#Connect-PnPOnline -Url $(devSiteUrl) -ClientId $(appid) -CertificateBase64Encoded $(baseencodedstring) -CertificatePassword (ConvertTo-SecureString -AsPlainText $(certpwd) -Force) -Tenant $(tenant)
+
+#Add-PnPApp -Scope Site -Publish -Overwrite -Path "$(System.DefaultWorkingDirectory)/$(Release.PrimaryArtifactSourceAlias)/drop/sharepoint/solution/spfx-demo.sppkg"
