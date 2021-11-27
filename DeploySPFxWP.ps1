a8dd4dfc4108124ab041e9ea6c9ab5d854570f07
SandeepDev365 <93072655+SandeepDev365@users.noreply.github.com>
2021-11-27 20:27:49 +0530

    connectpnp

--- DeploySPFxWP.ps1
+++ DeploySPFxWP.ps1
@@ -1,10 +1,10 @@
 ﻿Write-Output "Executing PowerShell Script..."
 
-$test = $Env:DEVSITEURL
-Write-Output "Test secret - $test"
+Write-Output "Installing PnP module..."
+Install-Module -Name PnP.PowerShell -Force -Scope "CurrentUser"
 
-#Install-Module -Name PnP.PowerShell -Force -Scope "CurrentUser"
-
-#Connect-PnPOnline -Url $(devSiteUrl) -ClientId $(appid) -CertificateBase64Encoded $(baseencodedstring) -CertificatePassword (ConvertTo-SecureString -AsPlainText $(certpwd) -Force) -Tenant $(tenant)
+Write-Output "Connect PnP Online..."
+Connect-PnPOnline -Url $Env:DEVSITEURL -ClientId $Env:APPID -CertificateBase64Encoded $Env:BASEENCODEDSTRING -CertificatePassword (ConvertTo-SecureString -AsPlainText $Env:CERTPWD -Force) -Tenant $Env:TENANT
+Write-Output "Connect PnP Successful"
 
 #Add-PnPApp -Scope Site -Publish -Overwrite -Path "$(System.DefaultWorkingDirectory)/$(Release.PrimaryArtifactSourceAlias)/drop/sharepoint/solution/spfx-demo.sppkg"
